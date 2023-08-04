343cfaab06fee1abb778a5f19afa01ba29940b54
Avas <avas.bajracharya@renegadeinsurance.com>
2023-08-04 10:50:19 +0545

    refctor: moved onload contents of unpkg plugin in a separate file

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,50 +1,29 @@
-import axios from 'axios';
 import * as esbuild from 'esbuild-wasm';
-import localforage from 'localforage';
 
-const fileCache = localforage.createInstance({
-  name: "fileCache"
-});
-
-export const unpkgPathPlugin = (input: string) => {
+export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        if(args.path === "index.js"){
-          return { path: args.path, namespace: 'a' };
-        }
-        if(args.path.includes("./") || args.path.includes("../")){
-          const formattedURL = new URL(args.path, "https://unpkg.com" + args.resolveDir + "/");
-          return {
-            namespace: "a",
-            path: formattedURL.href
-          }
-        }
-        return { path: `https://unpkg.com/${args.path}`, namespace: 'a' }
+      // Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
+            .href,
+        };
+      });
 
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: input,
-          };
-        }
-        const cachedResult = await localforage.getItem<esbuild.OnLoadResult>(args.path);
-        if(cachedResult){
-          return cachedResult
-        }
-        const {data, request} = await axios.get(args.path);
-        const result: esbuild.OnLoadResult = {
-          loader: "jsx",
-          contents: data,
-          resolveDir: new URL("./", request.responseURL).pathname
-        }
-        localforage.setItem(args.path, result);
+      // Handle main file of a module
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
       });
     },
   };
