aa3bb4819d23aa5678c5ab43e010567699f828b3
Avas <avas.bajracharya@renegadeinsurance.com>
2023-08-18 13:09:18 +0545

    feat: added prettier format and config options for pretiier formatter

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,5 +1,7 @@
 import { Editor } from "@monaco-editor/react";
-import React from "react";
+import React, { Fragment, useRef } from "react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
 
 interface EditorProps {
   initialValue: string;
@@ -7,25 +9,53 @@ interface EditorProps {
 }
 
 const CodeEditor = ({ initialValue, handleChange }: EditorProps) => {
+  const editorRef = useRef<any>(null);
+  const onEditorMountChange = (editor: any) => {
+    editorRef.current = editor;
+  };
+
+  const onFormatClick = () => {
+    if (editorRef.current) {
+      // get current value from editor
+      const unformatted = editorRef.current.getModel().getValue();
+
+      // format that value
+      const formatted = prettier.format(unformatted, {
+        parser: "babel",
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      });
+
+      // set the formatted value back in the editor
+      editorRef.current.setValue(formatted);
+    }
+  };
+
   return (
-    <Editor
-      onChange={(value) => handleChange(value)}
-      value={initialValue}
-      height="500px"
-      language="javascript"
-      theme="vs-dark"
-      options={{
-        wordWrap: "on",
-        minimap: {
-          enabled: false,
-        },
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-    />
+    <Fragment>
+      <button onClick={onFormatClick}>Format</button>
+      <Editor
+        onMount={onEditorMountChange}
+        onChange={(value) => handleChange(value)}
+        value={initialValue}
+        height="500px"
+        language="javascript"
+        theme="vs-dark"
+        options={{
+          wordWrap: "on",
+          minimap: {
+            enabled: false,
+          },
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </Fragment>
   );
 };
 
